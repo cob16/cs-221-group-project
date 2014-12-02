baec4dfcc79db214dfe93555fd8e4b42bd18816a
Scott <scl10@aber.ac.uk>
2014-12-02 19:03:27 +0000

    Web Implementation
    Updated the method for the website

--- src/WebInterfaceImplementation.php
+++ src/WebInterfaceImplementation.php
@@ -30,91 +30,84 @@
 
 
 <?php
-	public class UpdaterObject implements Updater{
-	
-		public function addReserve($resName){
-			//The new ID number is equal to the last ID value added + 1.
-			$resID = SCOPE_IDENTITY();
-			$resID = $resID + 1;
-			
-			INSERT INTO `Reserves` (`reserve_ID`, `reserve_name`) VALUES ('$resID', '$resName');
+	public function addReserve($resName){
+		//The new ID number is equal to the last ID value added + 1.
+		$ID = SELECT COUNT(*) FROM Reserves;
+		$ID = $ID + 1;
+		
+		INSERT INTO `Reserves` (`reserve_ID`, `reserve_name`) VALUES ('$resID', '$resName');
 
-			//==============================EXAMPLE TEST DATA====================================
-			//INSERT INTO `Reserves` (`reserve_ID`, `reserve_name`) VALUES (1, 'example reserve');
-			//===================================================================================
-		}
+		//==============================EXAMPLE TEST DATA====================================
+		//INSERT INTO `Reserves` (`reserve_ID`, `reserve_name`) VALUES (1, 'example reserve');
+		//===================================================================================
+	}
+	
+	public function addUser($auth, $email){
+		//Simply adds the input author and their email into the database
+		INSERT INTO `Users` (`author` , `Email`) VALUES ('$auth', '$email');
+	}
+	
+	public function addRecord($spe, $comment, $reserveName){
+	
+		//The new ID number is equal to the last ID value added + 1.
+		$ID = SELECT COUNT(*) FROM Recordings;
+		$ID = $ID + 1;
 		
-		public function addUser($auth, $email){
-			INSERT INTO `Users` (`author` , `Email`) VALUES ('$auth', '$email');
-		}
+		//Creates variable for todays date.
+		$date = date("Y-m-d");
 		
-		public function addRecord($spe, $comment, $reserveName){
+		//Whatever the path name is for the general
+		$photoPathGen = "C:// ...";
 		
-			//The new ID number is equal to the last ID value added + 1.
-			$ID =  SCOPE_IDENTITY();
-			$ID = $ID + 1;
-			
-			//Creates variable for todays date.
-			$date = date("Y-m-d");
-			
-			//Whatever the path name is for the general
-			$photoPathGen = "C:// ...";
-			
-			//Whatever the path name is for the species
-			$photoPathSpe = "C:// ..."
-			
-			//Should come from the users table. No table
-			$author_ID = "";
-			
-			
-			INSERT INTO `Recordings` (`ID`, `species`, `comments`, `date_recorded`, `photo_path_general`, `photo_path_species`, `author_ID`, `reserve_name`) 
-			VALUES ('$ID', '$spe', '$comment', '$date', '$photoPathGen', '$photoPathSpe' , '$author_ID', '$reserveName');
-			
-			//==================================================EXAMPLE TEST DATA ==================================================
-			//INSERT INTO `Recordings` (`ID`, `species`, `comments`, `date_recorded`, `photo_path_general`, `photo_path_species`, `author_ID`, `reserve_name`) VALUES
-			//(1, 'Abelia uniflora', 'this is the firts test record', '2014-11-16', 'foo/foo.jpeg', 'foo/foo1.jpeg', 1, '1');
-			//=======================================================================================================================
-		}
+		//Whatever the path name is for the species
+		$photoPathSpe = "C:// ..."
 		
+		$author_ID = "";
 		
-		public function addSpecies($Spec, $comName){
-			
-			//The new ID number is equal to the last ID value added + 1.
-			$ID =  SCOPE_IDENTITY();
-			$ID = $ID + 1;
-			
-			INSERT INTO `Species` (`species_id`, `Species`, `Common Name`) VALUES ('$ID', '$Spec', $comName);
-			
-			//========================================EXAMPLE TEST DATA ===============================
-			//INSERT INTO `Species` (`species_id`, `Species`, `Common Name`) VALUES(1, 'Abelia uniflora', NULL)
-			//=========================================================================================
-		}
+		INSERT INTO `Recordings` (`ID`, `species`, `comments`, `date_recorded`, `photo_path_general`, `photo_path_species`, `author_ID`, `reserve_name`) 
+		VALUES ('$ID', '$spe', '$comment', '$date', '$photoPathGen', '$photoPathSpe' , '$author_ID', '$reserveName');
 		
-		public function removeReserve(){
-			//========================================
-			//DON'T KNOW HOW TO DO THIS USING
-			//CHECKBOXES.
-			//=======================================
-		}
+		//==================================================EXAMPLE TEST DATA ==================================================
+		//INSERT INTO `Recordings` (`ID`, `species`, `comments`, `date_recorded`, `photo_path_general`, `photo_path_species`, `author_ID`, `reserve_name`) VALUES
+		//(1, 'Abelia uniflora', 'this is the firts test record', '2014-11-16', 'foo/foo.jpeg', 'foo/foo1.jpeg', 1, '1');
+		//=======================================================================================================================
+	}
+	
+	
+	public function addSpecies($Spec, $comName){
 		
-		public function removeRecord(){
-			//=====================================
-			//NOT SURE THIS IS AN ACTUAL REQUIRMENT 
-			//=====================================
-		}
+		//The new ID number is equal to the last ID value added + 1.
+		$ID = SELECT COUNT(*) FROM Species;
+		$ID = $ID + 1;
 		
-		public function removeUser(){
-			//=====================================
-			//NOT SURE THIS IS AN ACTUAL REQUIRMENT 
-			//=====================================
-		}
+		INSERT INTO `Species` (`species_id`, `Species`, `Common Name`) VALUES ('$ID', '$Spec', $comName);
 		
-		public function findRecord(){
-			//=============================================
-			//NOT CLEAR WHAT SEARCH PARAMETER TO USE.
-			//ALSO THE WEB UI TABLE SHOWS DISPLAYING THE 
-			//ABUDANCE. NO ABUNDANCE RECORD IN RECORDINGS.
-			//=============================================
-		}
+		//========================================EXAMPLE TEST DATA =======================================
+		//INSERT INTO `Species` (`species_id`, `Species`, `Common Name`) VALUES(1, 'Abelia uniflora', NULL)
+		//=================================================================================================
+	}
+	
+	public function removeReserve($reserveName){
+
+		//Deletes the reserve where the user inputs the reserves name.
+		DELETE FROM `Reserves` WHERE reserve_name='$reserveName';
+
+	}
+	
+	public function removeRecord($species, $reserveName){
+
+		//Deletes the record where the user input the reserve name and the species to remove. 
+		DELETE FROM `Recordings` WHERE reserve_name='$reserveName' AND species='$species';
+
+	}
+	
+	public function removeUser($Email){
+		//Deletes a user where the input email is the users email. 
+		DELETE FROM `users` WHERE Email = '$Email';
+	}
+	
+	public function findRecord($species, $reserveName){
+		$SQLSearch = SELECT * FROM `Recordings` WHERE reserve_name='$reserveName' AND species='$species';
+		return $SQLSearch;
 	}
 ?> 
