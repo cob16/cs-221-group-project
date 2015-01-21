c984beec788d8f083c2bdfe670ece4a16010b433
Zach Yewman <zach.yewman@gmail.com>
2015-01-21 11:21:25 +0000

    reserve name at top of list, replaces reserve name in table

--- src/RPSView/public_html/records.php
+++ src/RPSView/public_html/records.php
@@ -10,94 +10,106 @@
 <body>
 	<div class="header">
 		<h1><a href="https://cormacbrady.info/~tkek"><strong>RPSR</strong>view</a></h1>
-		<p>database of reserves</p>
+		<p>Record List</p>
 		<a class="gitlogo" href="https://github.com/cob16/cs-221-group-project"> <img src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" height="45" width="45"></a>
 	</div>
 
 	
 	<div class="container">
 
+
+
 		<?php
-		$reserve_name = $_GET["reserve"];
-		print($reserve_name);
+		/*
+		 * Takes the reserve id passed to it
+		 * anf looks it up in order to display
+		 * a table of the first 25 results
+		 */
+
+		$reserve_name = (int) $_GET["reserve"];
 		$host = 'localhost';
 		$user = 'tkek';
 		$pass = 'topkek3';
 		$database = 'cb-group-project';
 		$conn = new mysqli($host, $user, $pass, $database);
+
+
 		if(!$conn->connect_errno > 0){
 			$res = <<<SQL
 			SELECT *
 			FROM `Recordings`
+			WHERE $reserve_name = reserve_name
 SQL;
 			$dopefish = <<<SQL
 			SELECT *
 			FROM `Species`
 SQL;
-	
 
 			$fishsticks = <<<SQL
 			SELECT *
 			FROM `Reserves`
-			
+
 SQL;
 
-		
-
-			
 			if(!$res = $conn->query($res)){
 				die('There was an error running the query :)');
-		}
-
+			}
 			if(!$fishsticks = $conn->query($fishsticks)) {
 				die('there was an error running the query :(');
 			}
-
 			if(!$dopefish = $conn->query($dopefish)) {
 				die('there was an error running the query :(');
 			}
 
-		
+			// $resNumber = $a["reserve_name"];
+				while ($b = $fishsticks->fetch_assoc()) {
+					if ($reserve_name == $b["reserve_ID"]) {
+						echo '<h1>' . $b["reserve_name"] . '</h1>';
 
-		echo '<table cellpadding="25">';
-		echo '<thead>';
-		echo '<tr>';
-		echo '<th>Species</th>';
-		echo '<th>DAFOR Scale</th>';
-		echo '<th>Comments</th>';
-		echo '<th>Date Recorded</th>';
-		echo '<th>Location Photo</th>';
-		echo '<th>Species Photo</th>';
-		echo '<th>Email</th>';
-		echo '<th>Reserve Name</th>';
-		echo '</tr>';
-		while ($a = $res->fetch_assoc()) {
-		echo '<tr>';
-		$specNumber = $a["species"];
-		while ($c = $dopefish->fetch_assoc()) {
-			if ($specNumber == $c["species_id"]) {
-				echo '<td>' . $c["Species"] . '</td>';
+					}
+				}
+			// echo '<h1>Testing</h1>';
+			
+			echo '<table cellpadding="25">';
+			echo '<thead>';
+			echo '<tr>';
+			echo '<th>Species</th>';
+			echo '<th>DAFOR Scale</th>';
+			echo '<th>Comments</th>';
+			echo '<th>Date Recorded</th>';
+			echo '<th>Location Photo</th>';
+			echo '<th>Species Photo</th>';
+			echo '<th>Email</th>';
+			echo '</tr>';
+			//print eatch row
+			while ($a = $res->fetch_assoc()) {
+				echo '<tr>';
 
+				 // $specNumber = $a["species"];
+				 // echo $specNumber . ", ";
+				 // while ($c = $dopefish->fetch_assoc()) {
+				 	 // echo $specNumber . ", ";
+				 	// if ($specNumber == $c["species_id"]) {
+				 		// echo $specNumber . ", ";
+				 		// echo '<td>' . $c["Species"] . '</td>';
+				 	// }
+				 // }
+				echo '<td>' . $a["species"] . '</td>';
+				echo '<td>' . $a["DAFOR"] . '</td>';
+				echo '<td>' . $a["comments"] . '</td>';
+				echo '<td>' . $a["date_recorded"] . '</td>';
+				echo '<td><img src="' . $a["photo_path_general"] . '"></td>';
+				echo '<td><img src="' . $a["photo_path_species"] . '"</td>';
+				echo '<td>' . $a["Email"] . '</td>';
+				echo '</tr>';
 			}
+			echo '</table>';
 		}
-		echo '<td>' . $a["DAFOR"] . '</td>';
-		echo '<td>' . $a["comments"] . '</td>';
-		echo '<td>' . $a["date_recorded"] . '</td>';
-		echo '<td><img src="' . $a["photo_path_general"] . '"></td>';
-		echo '<td><img src="' . $a["photo_path_species"] . '"</td>';
-		echo '<td>' . $a["Email"] . '</td>';
-		$resNumber = $a["reserve_name"];
-		while ($b = $fishsticks->fetch_assoc()) {
-			if ($resNumber == $b["reserve_ID"]) {
-				echo '<td>' . $b["reserve_name"] . '</td>';
-
-			}
+		else
+		{
+			echo "connection error";
 		}
-		echo '</tr>';
-	}
-	echo '</table>';
-} 
-?>
+		?>
 
 
 </div>
