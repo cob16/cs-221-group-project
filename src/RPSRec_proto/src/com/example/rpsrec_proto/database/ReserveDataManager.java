e38ffebf8f5d2ad90778562c082cf216f0401a74
hmh7 <henryhollingsworth@henrys-mac.home>
2015-01-22 00:37:44 +0000

    sql stuffs

---
+++ src/RPSRec_proto/src/com/example/rpsrec_proto/database/ReserveDataManager.java
@@ -0,0 +1,47 @@
+package com.example.rpsrec_proto.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ReserveDataManager extends SQLiteOpenHelper {
+
+	private static final String DATABASE_NAME = "reserveDB";
+	private static final int DATABASE_VERSION = 1;
+	public static final String KEY_ID = "ID";
+	public static final String KEY_NAME = "keyName";
+	public static final String KEY_LOCATION = "keyLocation";
+
+	private static final String RESERVE_TABLE_NAME = "reserve";
+	private static final String RESERVE_TABLE_CREATE = "CREATE TABLE "
+			+ RESERVE_TABLE_NAME + " (" + KEY_ID
+			+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_NAME + " TEXT, "
+			+ KEY_LOCATION + " TEXT);";
+
+	public ReserveDataManager(Context context) {
+
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(RESERVE_TABLE_CREATE);
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void getWritableDataBase() {
+
+	}
+
+	public void getReadableDataBase() {
+
+	}
+
+}
